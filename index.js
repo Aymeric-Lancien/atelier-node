64aecf05f2a1cfdd803a0749fbf95723e9b43f29
Aymeric-Lancien <aymericlancien12@gmail.com>
2020-10-01 16:22:56 +0200

    exo node 2

---
+++ index.js
@@ -0,0 +1,6 @@
+const chalk = require("chalk");
+
+console.log(chalk.red("Michel 1"));
+console.log(chalk.blue("Michel 2"));
+console.log(chalk.green("Michel 3"));
+console.log(chalk.yellow("Michel 4"));
